06293ca2e784c5caad434ab8e13dd1fbc79f4902
Nico <nicolas.romero.f@usach.cl>
2019-05-18 21:27:39 -0400

    appname

--- seedsml.R
+++ seedsml.R
@@ -1,62 +1,62 @@
+
+
+
+library(readr)  
+library(dplyr)  
+library(plyr) 
+library(plumber)
+library(SparkR)
+
+sparkR.session(appName = "seedsml")
+
+
+
+url <- "https://archive.ics.uci.edu/ml/machine-learning-databases/00236/seeds_dataset.txt"
   
+df <-  
+  read.delim(url, sep="")
+df <- dplyr::mutate(df, id = as.integer(rownames(df)))
+colnames(df) <- c("area", "perimeter", "compactness", "lengthk" ,"widthk", "asymmetry", "lengthkg", "class", "id")
+
+df <- dplyr::select(df,-id)
+
+# 
+
+
+ddf <- createDataFrame(df)
+seed <- 12345
+training_ddf <- sample(ddf, withReplacement=FALSE, fraction=0.7, seed=seed)
+test_ddf <- except(ddf, training_ddf)
+model <- spark.randomForest(training_ddf, class ~ ., type="classification", seed=seed)
+summary(model)
+
+
+# 
+# actual_vs_predicted <-  
+#   dplyr::inner_join(df, prediction_df, "id") %>%  
+#   dplyr::select(id, actual = taste, predicted = prediction)
+# 
+# mean(actual_vs_predicted$actual == actual_vs_predicted$predicted)
+# 
+# tablePred = table(actual_vs_predicted$actual, actual_vs_predicted$predicted)
+# 
+# 
+#* Echo back the input
+#* @param msg The message to echo
+#* @get /echo
+function(msg=""){
+  list(msg = paste0("The message is: '", msg, "'"))
+}
+
+#* Echo back the input
+#* @get /predicted
+function(){
+  l <- list(a = list(area = 14.88, perimeter =	14.57, compactness =	0.8811, lengthk =	5.554, widthk =	3.333, asymmetry =	1.0180, lengthkg =	4.956))
+  test <- ldply (l, data.frame)
+  test <- dplyr::select(test,-.id)
+  test <- dplyr::mutate(test, id = as.integer(rownames(test)))
+  test <- createDataFrame(test)
   
-  
-  library(readr)  
-  library(dplyr)  
-  library(plyr) 
-  library(plumber)
-  library(SparkR)
-  
-  sparkR.session(master = "spark://localhost:7077")
-  
-  
-  
-  url <- "https://archive.ics.uci.edu/ml/machine-learning-databases/00236/seeds_dataset.txt"
-    
-  df <-  
-    read.delim(url, sep="")
-  df <- dplyr::mutate(df, id = as.integer(rownames(df)))
-  colnames(df) <- c("area", "perimeter", "compactness", "lengthk" ,"widthk", "asymmetry", "lengthkg", "class", "id")
-  
-  df <- dplyr::select(df,-id)
-  
-  # 
-  
-  
-  ddf <- createDataFrame(df)
-  seed <- 12345
-  training_ddf <- sample(ddf, withReplacement=FALSE, fraction=0.7, seed=seed)
-  test_ddf <- except(ddf, training_ddf)
-  model <- spark.randomForest(training_ddf, class ~ ., type="classification", seed=seed)
-  summary(model)
-  
-  
-  # 
-  # actual_vs_predicted <-  
-  #   dplyr::inner_join(df, prediction_df, "id") %>%  
-  #   dplyr::select(id, actual = taste, predicted = prediction)
-  # 
-  # mean(actual_vs_predicted$actual == actual_vs_predicted$predicted)
-  # 
-  # tablePred = table(actual_vs_predicted$actual, actual_vs_predicted$predicted)
-  # 
-  # 
-  #* Echo back the input
-  #* @param msg The message to echo
-  #* @get /echo
-  function(msg=""){
-    list(msg = paste0("The message is: '", msg, "'"))
-  }
-  
-  #* Echo back the input
-  #* @get /predicted
-  function(){
-    l <- list(a = list(area = 14.88, perimeter =	14.57, compactness =	0.8811, lengthk =	5.554, widthk =	3.333, asymmetry =	1.0180, lengthkg =	4.956))
-    test <- ldply (l, data.frame)
-    test <- dplyr::select(test,-.id)
-    test <- dplyr::mutate(test, id = as.integer(rownames(test)))
-    test <- createDataFrame(test)
-    
-    predictions <- predict(model, test)
-    prediction_df <- collect(select(predictions, "id", "prediction"))
-  }
+  predictions <- predict(model, test)
+  prediction_df <- collect(select(predictions, "id", "prediction"))
+}
