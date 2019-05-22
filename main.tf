6b52bb22db7e9191488b293f025cde8722e4c328
Richard Torti <richard.torti@usach.cl>
2019-05-22 01:09:23 -0400

    updating tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ provider "google" {
 #Crear una nueva instancia
 resource "google_compute_instance" "default" {
  name         = "seedsml"
- machine_type = "n1-standard-4"
+ machine_type = "n1-standard-1"
  zone         = "us-west1-a"
 
  boot_disk {
@@ -26,3 +26,53 @@ network_interface {
 }
 }
 
+resource "google_dataproc_cluster" "Cluster1" {
+    name       = "Cluster1"
+    region     = "us-central1"
+
+    cluster_config {
+        staging_bucket        = "ML in seeds"
+
+        master_config {
+            num_instances     = 1
+            machine_type      = "n1-standard-1"
+            disk_config {
+                boot_disk_type = "pd-ssd"
+                boot_disk_size_gb = 15
+            }
+        }
+
+        worker_config {
+            num_instances     = 2
+            machine_type      = "n1-standard-1"
+            disk_config {
+                boot_disk_size_gb = 15
+                num_local_ssds    = 1
+            }
+        }
+
+        preemptible_worker_config {
+            num_instances     = 0
+        }
+
+        # Override or set some custom properties
+        software_config {
+            image_version       = "1.3.7-deb9"
+            override_properties = {
+                "dataproc:dataproc.allow.zero.workers" = "true"
+            }
+        }
+
+        gce_cluster_config {
+            #network = "${google_compute_network.dataproc_network.name}"
+            tags    = ["foo", "bar"]
+        }
+
+        # You can define multiple initialization_action blocks
+        initialization_action {
+            script      = "gs://dataproc-initialization-actions/stackdriver/stackdriver.sh"
+            timeout_sec = 500
+        }
+
+    }
+}
